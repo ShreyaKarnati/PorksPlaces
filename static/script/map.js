572f6199ced7968fda344c598c80a4d4f7e63b3f
Rohanpanigrahi <108543227+Rohanpanigrahi@users.noreply.github.com>
2023-05-06 14:11:52 -0700

    Add files via upload

--- static/script/map.js
+++ static/script/map.js
@@ -1,7 +1,7 @@
-//Google Map variable.
+// Google Map variable.
 let map;
 
-//content for Markers.
+// Content for Markers.
 const contents = [
     [47.6628, -122.3139, "<h1 class = \"mapLocations\"><a href = https://www.udistrictseattle.com/ target = \"blank\" class = \"mapLinks\">University District</a></h1> <p class = \"mapTexts\">A social hub for all UW Students. There are beautiful trails such as the Burke Gillman Trail. Eateries such as Mod Pizza, Cedars, and Aladdin.</p>"],
     [47.6364, -122.2958, "<h1 class = \"mapLocations\"><a href=https://botanicgardens.uw.edu/washington-park-arboretum/ target = \"blank\" class = \"mapLinks\">The Arboretum</a></h1> <p class = \"mapTexts\">A welcome oasis on the shores of Lake Washington. Take a walk through the Arboretum and discover this beautiful living collection. The Graham Visitors Center contains an information desk, and a gift shop managed by the Arboretum Foundation. The Japanese Garden, located at the south end of the Arboretum has an entrance fee and is managed by the City of Seattle.</p>"],
@@ -22,39 +22,37 @@ const contents = [
     [47.6684, -122.3509, "<h1 class = \"mapLocations\">Woodland Park Zoo</h1> <p class = \"mapTexts\">"]
 ];
 
-//Create Map
+// Create Map
 async function initMap() {
     const { Map } = await google.maps.importLibrary("maps");
 
-    map = new Map(document.getElementById("map"), 
-    {
-        center: { lat: 47.6628, lng: -122.3139 },
-        zoom: 10
-    });
+    map = new Map(document.getElementById("map"),
+        {
+            center: { lat: 47.6628, lng: -122.3139 },
+            zoom: 10
+        });
 
-    //Create Markers.
-    for (let i = 0; i < contents.length; i++)
-    {
-        add_marker(contents[i][0],contents[i][1],contents[i][2]);
+    // Create Markers.
+    for (let i = 0; i < contents.length; i++) {
+        add_marker(contents[i][0], contents[i][1], contents[i][2]);
     }
 };
 
-//Function for adding markers, marker info, and event listeners.
-function add_marker(latitude, longitude, description)
-{
-    var marker = new google.maps.Marker ({
-        position:{ lat: latitude, lng: longitude },
-        map:map
+// Function for adding markers, marker info, and event listeners.
+function add_marker(latitude, longitude, description) {
+    var marker = new google.maps.Marker({
+        position: { lat: latitude, lng: longitude },
+        map: map
     });
 
-    var marker_info = new google.maps.InfoWindow ({
+    var marker_info = new google.maps.InfoWindow({
         content: description
     });
 
-    marker.addListener('click',function() {
+    marker.addListener('click', function () {
         marker_info.open(map, marker);
     });
 };
 
-//Create map.
+// Create map.
 initMap();
